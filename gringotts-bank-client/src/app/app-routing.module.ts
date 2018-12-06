c7e94f18ac36daa2b7a0132cedd7a15029d761b3
jonathas-garcia <jonathas.cgarcia@gmail.com>
2018-12-06 19:09:07 -0200

    Colocado autenticação nas rotas e routerLink do navbar.

--- gringotts-bank-client/src/app/app-routing.module.ts
+++ gringotts-bank-client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './services/login/login.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -9,11 +11,11 @@ import { CreateAccountComponent } from './components/create-account/create-accou
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'statement', component: StatementComponent },
-  { path: 'transfer', component: TransferComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'create-account', component: CreateAccountComponent }
-
+  { path: 'login', component: LoginComponent},
+  { path: 'statement', component: StatementComponent, canActivate: [LoginGuard]  },
+  { path: 'transfer', component: TransferComponent, canActivate: [LoginGuard]  },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [LoginGuard]  },
+  { path: 'create-account', component: CreateAccountComponent, canActivate: [LoginGuard] }
 ];
 
 @NgModule({
