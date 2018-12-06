c3ce8321f18cfbc51ec206fc0becfa7c50a00fdf
Murilo Medeiros <murilo_medeiros98@hotmail.com>
2018-12-06 02:26:56 -0200

    Rotas feitas

--- gringotts-bank-client/src/app/app-routing.module.ts
+++ gringotts-bank-client/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StatementComponent } from './components/statement/statement.component';
+import { TransferComponent } from './components/transfer/transfer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'statement', component: StatementComponent },
+  { path: 'transfer', component: TransferComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'create-account', component: CreateAccountComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
